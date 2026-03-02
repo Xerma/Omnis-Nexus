ec57728fff0a5fed6f26fda6c61b8df664325a09
cheave2 <cheave2@wgu.edu>
2026-03-01 23:28:52 -0600

    Initial Blazor Server project setup

---
+++ Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,8 @@
+namespace OmnisNexus.Components.Account
+{
+    public class PasskeyInputModel
+    {
+        public string? CredentialJson { get; set; }
+        public string? Error { get; set; }
+    }
+}
---
+++ Components/Account/PasskeyOperation.cs
@@ -0,0 +1,8 @@
+namespace OmnisNexus.Components.Account
+{
+    public enum PasskeyOperation
+    {
+        Create = 0,
+        Request = 1,
+    }
+}

c8c6538f3876b71dd2c4241f15a6b1a5b2e194c0
cheave2 <cheave2@wgu.edu>
2026-03-24 10:36:15 -0500

    Phase 3: Added a more reliable model for roles instead of using strings everywhere

---
+++ Models/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace OmnisNexus.Models
+{
+    public static class Roles
+    {
+        public const string Owner = "Owner";
+        public const string Moderator = "Moderator";
+        public const string Member = "Member";
+    }
+}

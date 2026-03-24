0c1e94edb7dbb07392503fe3ff02f75c9a52d6b2
cheave2 <cheave2@wgu.edu>
2026-03-24 10:37:08 -0500

    Phase 3: Added a service that handles searching through all messages in a community based on provided text

---
+++ Services/CommunitySearchService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OmnisNexus.Data;
+using OmnisNexus.Models;
+
+namespace OmnisNexus.Services
+{
+    public class CommunitySearchService
+    {
+        private readonly IDbContextFactory<ApplicationDbContext> _dbContextFactory;
+
+        public CommunitySearchService(IDbContextFactory<ApplicationDbContext> dbFactory)
+        {
+            _dbContextFactory = dbFactory;
+        }
+
+        public async Task<Dictionary<string, List<SearchItem>>> SearchMessages(Guid communityId, string search)
+        {
+            using var db = await _dbContextFactory.CreateDbContextAsync();
+
+            List<SearchItem> searchItems = await db.Messages
+                .Where(m => m.Content.Contains(search) && m.Channel.CommunityId == communityId)
+                .OrderByDescending(m => m.CreatedAt)
+                .Select(m => new SearchItem
+                {
+                    MessageId = m.Id,
+                    ChannelId = m.ChannelId,
+                    ChannelName = m.Channel.Name,
+                    AuthorName = m.User.UserName,
+                    MessageContent = m.Content,
+                    CreatedAt = m.CreatedAt,
+                    UpdatedAt = (m.UpdatedAt == null) ? null : m.UpdatedAt
+                })
+                .ToListAsync();
+
+            Dictionary<string, List<SearchItem>> searchItemDict = searchItems
+                .GroupBy(item => item.ChannelName)
+                .ToDictionary(group => group.Key, group => group
+                .ToList());
+
+            return searchItemDict;
+        }
+    }
+}

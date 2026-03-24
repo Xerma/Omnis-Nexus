dfef040ba3a8d47c89ea138b419ba69107536995
cheave2 <cheave2@wgu.edu>
2026-03-24 10:40:59 -0500

    Phase 3: Added an interface for PermissionService for better abstraction

---
+++ Services/IPermissionService.cs
@@ -0,0 +1,14 @@
+﻿using OmnisNexus.Models;
+
+namespace OmnisNexus.Services
+{
+    public interface IPermissionService
+    {
+        bool CanDeleteMessage(Membership membership, Message message);
+        bool CanEditMessage(Membership membership, Message message);
+        bool CanManageChannels(Membership membership);
+        bool CanManageCommunity(Membership membership);
+        bool CanLeaveCommunity(Membership membership);
+        bool CanManageMembers(Membership membership);
+    }
+}

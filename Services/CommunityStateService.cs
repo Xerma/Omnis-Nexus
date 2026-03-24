f2be9d40c42aadb961cb79a1a7a7f4997a05970f
cheave2 <cheave2@wgu.edu>
2026-03-24 10:38:29 -0500

    Phase 4: The displayed community list is now handled by this service to set what communities should display and update the listing

---
+++ Services/CommunityStateService.cs
@@ -0,0 +1,20 @@
+﻿using OmnisNexus.Models;
+
+namespace OmnisNexus.Services
+{
+    public class CommunityStateService
+    {
+        public event Action? OnChange;
+
+        public List<Community> UserCommunities { get; private set; } = new();
+
+        public void SetCommunities(List<Community> communities)
+        {
+            UserCommunities = communities;
+            NotifyStateChanged();
+        }
+
+        public void NotifyStateChanged() => OnChange?.Invoke();
+
+    }
+}

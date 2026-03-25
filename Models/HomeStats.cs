d2a0f320a1b19304067feac76e8289dfdeca0275
cheave2 <cheave2@wgu.edu>
2026-03-24 21:08:09 -0500

    Phase 4: Added HomeStats from refactoring statistics into service for optimal data passing

---
+++ Models/HomeStats.cs
@@ -0,0 +1,10 @@
+﻿namespace OmnisNexus.Models
+{
+    public class HomeStats
+    {
+        public int CommunitiesJoined { get; set; }
+        public int MessagesSent { get; set; }
+        public int CommunitiesOwned { get; set; }
+        public int MessagesEdited { get; set; }
+    }
+}
